a8b47a418f768d6d79c1b463c6a543b7c528dbb1
shakao <shakao@microsoft.com>
2020-07-13 14:43:05 -0700

    Add sprites

---
+++ blob.ts
@@ -0,0 +1,6 @@
+namespace custom.blob {
+    //% fixedInstance jres blockIdentity=images._image
+    export const blob1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const blob2 = image.ofBuffer(hex``);
+}
